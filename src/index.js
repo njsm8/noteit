2e7921c03f5d3fd1fc831a674c25c79d120ee165
Nischit Shetty <njmshetty@gmail.com>
2021-05-01 15:35:39 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAPGmYRDyGzpzBDmIYLLIGpOVi5DohZCk0",
+  authDomain: "noteit-neog.firebaseapp.com",
+  projectId: "noteit-neog",
+  storageBucket: "noteit-neog.appspot.com",
+  messagingSenderId: "843232270044",
+  appId: "1:843232270044:web:bc2c2bf50b7f3415fb0619",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("evernote-container")
 );
 
 // If you want to start measuring performance in your app, pass a function
